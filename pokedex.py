1e6a761442677d2c8c45196c25c0156348c70e84
Nisrine <nisnouss8348@gmail.com>
2026-02-03 11:45:22 +0100

    Nom des variables modifiées en anglais

--- pokedex.py
+++ pokedex.py
@@ -3,42 +3,42 @@ from PIL import ImageTk, Image
 
 # Mise en place d'une classe pour répertorier les variables ainsi que leurs attributs
 class Pokedex:
-    def __init__(self, nom, type, capacité, force):
-        self.name = nom
+    def __init__(self, name, type, ability, strenght):
+        self.name = name
         self.type = type
-        self.ability = capacité
-        self.strenght = force
+        self.ability = ability
+        self.strenght = strenght
 
-    def Afficher_infos(self):
+    def Display_infos_pokemon(self):
         print(f"Nom : {self.name}")
         print(f"Type : {self.type}")
         print(f"Capacité : {self.ability}")
         print(f"Force : {self.strenght}")
 
 # Ouverture de la fenêtre principale
-fenetre = tk.Tk()
-fenetre.title("Projet d'évaluation - Pokedex")
-fenetre.geometry("720x480")
+window = tk.Tk()
+window.title("Projet d'évaluation - Pokedex")
+window.geometry("720x480")
 
 # Afficher les informations des pokemons sur une autre fenêtre ainsi que leur photo
-def Afficher_infos(event):
-    fenetre_infos = tk.Toplevel()
-    fenetre_infos.geometry("820x580")
-    fenetre_infos.title("Infos sur les pokemon")
-    label_infos = tk.Label(fenetre_infos)
+def Display_infos(event):
+    window_info = tk.Toplevel()
+    window_info.geometry("820x580")
+    window_info.title("Infos sur les pokemon")
+    label_infos = tk.Label(window_info)
     try:
         selection = menu.get(menu.curselection())
         print(selection)
         if selection == "Rondoudou":
-            Rondoudouimg_label = tk.Label(fenetre_infos, image=Rondoudouimg)
+            Rondoudouimg_label = tk.Label(window_info, image=Rondoudouimg)
             Rondoudouimg_label.pack()
             label_infos.config(text=f"{Pokemon1.name} est de type {Pokemon1.type}, a pour capacité le pouvoir de la {Pokemon1.ability}, et a {Pokemon1.strenght} XP de force")
         elif selection == "Salamèche":
-            Salamecheimg_label = tk.Label(fenetre_infos, image=Salamecheimg)
+            Salamecheimg_label = tk.Label(window_info, image=Salamecheimg)
             Salamecheimg_label.pack()
             label_infos.config(text=f"{Pokemon2.name} est de type {Pokemon2.type}, a pour capacité le pouvoir du {Pokemon2.ability}, et a {Pokemon2.strenght} XP de force")
         elif selection == "Pikachu":
-            Pikachuimg_label = tk.Label(fenetre_infos, image=Pikachuimg)
+            Pikachuimg_label = tk.Label(window_info, image=Pikachuimg)
             Pikachuimg_label.pack()
             label_infos.config(text=f"{Pokemon3.name} est de type {Pokemon3.type}, a pour capacité le pouvoir de l'{Pokemon3.ability}, et a {Pokemon3.strenght} XP de force")
     except:
@@ -70,7 +70,7 @@ def Afficher_selection(event):
         Informations_pokemon.config(text="Veuillez selectionner un pokemon")
 
 # Création du menu déroulant qui va répertorier les informations des pokemon
-menu = tk.Listbox(fenetre, selectmode=tk.SINGLE)
+menu = tk.Listbox(window, selectmode=tk.SINGLE)
 menu.pack()
 menu.bind("<Button-1>", Afficher_selection)
 
@@ -79,11 +79,11 @@ for option in options:
     menu.insert(tk.END, option)
 
 # Bouton permettant d'ouvrir une nouvelle fenêtre affichant les informations sur les pokemons
-print_button = tk.Button(fenetre, text="Afficher les informations", command=Afficher_infos)
+print_button = tk.Button(window, text="Afficher les informations", command=Display_infos)
 print_button.pack()
-print_button.bind("<Button-1>", Afficher_infos)
+print_button.bind("<Button-1>", Display_infos)
 
-Informations_pokemon = tk.Label(fenetre, text= "Voici les informations sur le pokemon selectionné : ")
+Informations_pokemon = tk.Label(window, text= "Voici les informations sur le pokemon selectionné : ")
 Informations_pokemon.pack()
 
 # Ajout d'un pokemon au menu
@@ -93,36 +93,36 @@ def save_infos(event):
     fenetre_infos.title("Infos sur les pokemon")
     label_save = tk.Label(fenetre_infos)
     with open("NewPokemon.txt", "a") as file:
-        menu.insert(tk.END, ajout_pokemon_name.get()+ "\n")
-        label_save.config(text=f"Le nom du Pokemon est : {ajout_pokemon_name.get()}. Il est de type : {ajout_pokemon_type.get()}, ses capacités sont : {ajout_pokemon_ability.get()}, et sa force est de {ajout_pokemon_strenght.get()}")
+        menu.insert(tk.END, add_pokemon_name.get()+ "\n")
+        label_save.config(text=f"Le nom du Pokemon est : {add_pokemon_name.get()}. Il est de type : {add_pokemon_type.get()}, ses capacités sont : {add_pokemon_ability.get()}, et sa force est de {add_pokemon_strenght.get()}")
     label_save.pack()
 
-label_name = tk.Label(fenetre, text="Nom du Pokemon")
+label_name = tk.Label(window, text="Nom du Pokemon")
 label_name.pack()
 
-ajout_pokemon_name = tk.Entry(fenetre)
-ajout_pokemon_name.pack()
+add_pokemon_name = tk.Entry(window)
+add_pokemon_name.pack()
 
-label_type = tk.Label(fenetre, text="Type de Pokemon")
+label_type = tk.Label(window, text="Type de Pokemon")
 label_type.pack()
 
-ajout_pokemon_type = tk.Entry(fenetre)
-ajout_pokemon_type.pack()
+add_pokemon_type = tk.Entry(window)
+add_pokemon_type.pack()
 
-label_ability = tk.Label(fenetre, text="Capacités du Pokemon")
+label_ability = tk.Label(window, text="Capacités du Pokemon")
 label_ability.pack()
 
-ajout_pokemon_ability = tk.Entry(fenetre)
-ajout_pokemon_ability.pack()
+add_pokemon_ability = tk.Entry(window)
+add_pokemon_ability.pack()
 
-label_strenght = tk.Label(fenetre, text="Force du Pokemon")
+label_strenght = tk.Label(window, text="Force du Pokemon")
 label_strenght.pack()
 
-ajout_pokemon_strenght = tk.Entry(fenetre)
-ajout_pokemon_strenght.pack()
+add_pokemon_strenght = tk.Entry(window)
+add_pokemon_strenght.pack()
 
-btn_ajout = tk.Button(fenetre, text="Ajoutez un pokemon")
-btn_ajout.pack()
-btn_ajout.bind("<Button-1>", save_infos)
+btn_add = tk.Button(window, text="Ajoutez un pokemon")
+btn_add.pack()
+btn_add.bind("<Button-1>", save_infos)
 
-fenetre.mainloop()
+window.mainloop()
